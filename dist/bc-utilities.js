05ce7b16ba8c09a6501a0d527dcf5430493ccf9d
Kevin Adams <kevin@buttercoin.com>
2013-12-08 10:49:43 -0800

    rebase

--- dist/bc-utilities.js
+++ dist/bc-utilities.js
@@ -32,4 +32,35 @@
     return !isNaN(parseFloat(n)) && isFinite(n);
   };
 
+  root.isEmpty = function(obj) {
+    var key;
+    if ((obj == null) || obj.length === 0) {
+      return true;
+    }
+    if ((obj.length != null) && obj.length > 0) {
+      return false;
+    }
+    for (key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  root.keyForValue = function(needle, haystack) {
+    var k, v;
+    for (k in haystack) {
+      v = haystack[k];
+      if (v === needle) {
+        return k;
+      }
+    }
+    return void 0;
+  };
+
+  root.capitalize = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
 }).call(this);
